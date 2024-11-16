4c4ffb793fe4584f96395b012411bc5984721452
programmer3178 <shritejashastrys@gmail.com>
2024-11-16 13:23:38 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+
+public class Variables {
+
+    
+    public static void main(String[] args) {
+        float number1 = 3.5678f;
+        float number2 = 4.974974f;
+        System.out.println(number1 + number2);
+    }
+    
+}
