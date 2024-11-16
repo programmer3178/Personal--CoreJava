a1579223f3ba30c70ca9764d2da5e255fb3bc5ed
programmer3178 <shritejashastrys@gmail.com>
2024-11-16 14:49:45 +0530

    Add files via upload

---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implicittype.casting;
+
+
+public class ImplicitTypeCasting {
+
+    public static void main(String[] args) {
+       int number = 10;
+       double result = number;
+       System.out.println(number);
+    }
+    
+}
