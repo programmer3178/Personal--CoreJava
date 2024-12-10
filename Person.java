70808c625691f2aab3a38aa8c586a39a74a6f522
programmer3178 <shritejashastrys@gmail.com>
2024-12-10 11:38:24 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,61 @@
+package com.tnsif.constructor;
+
+/**
+ * @author sys
+ */
+//Entity class
+public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+    private float salary;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String gender, float salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", age=" + age + ", gender=" + gender + ", salary=" + salary + '}';
+    }
+
+}
